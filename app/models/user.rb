49ea3a85cea2859d1aa74b9e6ce393349f22e353
ToniCP <tonicp97@gmail.com>
2019-09-06 13:58:16 -0500

    Secondo commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User
   include Mongoid::Document
+
   field :email, type: String
   field :name, type: String
   field :last_name, type: String
   field :mobile_phone, type: String
+  field :work_place, type: String
+
+  #validates :email, :name, presence: true
+  validates_presence_of :email, :name
+  validates :email, uniqueness: true
+
+  def full_name
+    "#{name} #{last_name}"
+  end
 end
