0c18ecd2b5a7aa7072517ece5617cfec18d05c1b
ToniCP <tonicp97@gmail.com>
2019-09-09 12:37:59 -0500

    Tercero

---
+++ app/models/score.rb
@@ -0,0 +1,8 @@
+class Score
+  include Mongoid::Document
+
+  field :qualification, type: Float
+  field :subject, type: String
+
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,24 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
+  include PersonalInfo
+
+  has_many :scores
+  embeds_many :parents
 
   field :email, type: String
   field :name, type: String
   field :last_name, type: String
   field :mobile_phone, type: String
   field :work_place, type: String
+  field :age, type: Integer
+  field :is_active, type: Boolean
 
-  #validates :email, :name, presence: true
+  accepts_nested_attributes_for :scores
+
+  scope :adults, -> { where(age: {'$gte': 18} ) }
+  scope :recently_adults, -> { where(age: 18) }
+
   validates_presence_of :email, :name
   validates :email, uniqueness: true
 
